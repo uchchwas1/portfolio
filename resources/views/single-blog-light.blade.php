66105c6e3da88df25d5cc075188700c4297b660f
uchchwas1 <uchchwas.das@northsouth.edu>
2023-07-18 23:39:37 +0600

    insert full project

---
+++ resources/views/single-blog-light.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- metas -->
+    <meta charset="utf-8">
+    <meta name="author" content="themepaa">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Nairo - Portfolio Template">
+    <meta name="description" content="Nairo - Portfolio Template">
+    <!-- title -->
+    <title>Nairo - Portfolio Template</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <!-- plugin CSS -->
+    <link href="static/plugin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="static/plugin/font-awesome/css/all.min.css" rel="stylesheet">
+    <link href="static/plugin/et-line/style.css" rel="stylesheet">
+    <link href="static/plugin/themify-icons/themify-icons.css" rel="stylesheet">
+    <link href="static/plugin/owl-carousel/css/owl.carousel.min.css" rel="stylesheet">
+    <link href="static/plugin/magnific/magnific-popup.css" rel="stylesheet">
+    <link href="static/plugin/scroll/jquery.mCustomScrollbar.min.css" rel="stylesheet">
+    <!-- theme css -->
+    <link href="static/css/style.css" rel="stylesheet">
+</head>
+<!-- Body Start -->
+
+<body data-spy="scroll" data-target="#navbar-collapse-toggle" data-offset="70" class="theme-light">
+    <!-- page loading -->
+    <div id="loading">
+        <div class="load-circle"><span class="one"></span></div>
+    </div>
+    <!-- End -->
+    <!-- Header Start -->
+    <div class="mob-header">
+        <div class="d-flex">
+            <div class="navbar-brand">
+                <a class="logo-text" href="index.html">
+                    Raquel
+                </a>
+            </div>
+            <button class="toggler-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+    </div>
+    <!-- End Header -->
+    <!-- nav bar -->
+    <header class="header-left" id="navbar-collapse-toggle">
+        <div class="scroll-bar">
+            <div class="hl-top">
+                <div class="hl-logo">
+                    <a href="../index.html">
+                        N
+                    </a>
+                </div>
+            </div>
+            <ul class="nav nav-menu">
+                <li>
+                    <a class="nav-link" href="index-light.html#home" data-toggle="tooltip" title="HOME">
+                        <i class="fas fa-house-damage"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="index-light.html#about" data-toggle="tooltip" title="ABOUT">
+                        <i class="far fa-address-card"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="index-light.html#resume" data-toggle="tooltip" title="RESUME">
+                        <i class="far fa-file"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="index-light.html#work" data-toggle="tooltip" title="WORK">
+                        <i class="fas fa-briefcase"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="index-light.html#blog" data-toggle="tooltip" title="BLOG">
+                        <i class="fas fa-blog"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="index-light.html#contactus" data-toggle="tooltip" title="CONTACT">
+                        <i class="fas fa-id-card-alt"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <!-- End nav bar -->
+    <!-- Main Start -->
+    <main class="main-left">
+        <div class="single-blog">
+            <div class="container">
+                <div class="blog-feature-img">
+                    <img src="static/img/single-blog.jpg" title="" alt="">
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <article class="article">
+                            <div class="article-title">
+                                <h6><a href="#">Lifestyle</a></h6>
+                                <h2>They Now Bade Farewell To The Kind But Unseen People</h2>
+                                <div class="media">
+                                    <div class="avatar">
+                                        <img src="static/img/team-1.jpg" title="" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <label>Raquel Roth</label>
+                                        <span>26 FEB 2020</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="article-content">
+                                <p>Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem. Adipiscing veni amet luctus enim sem libero tellus viverra venenatis aliquam. Commodo natoque quam pulvinar elit.</p>
+                                <p>Eget aenean tellus venenatis. Donec odio tempus. Felis arcu pretium metus nullam quam aenean sociis quis sem neque vici libero. Venenatis nullam fringilla pretium magnis aliquam nunc vulputate integer augue ultricies cras. Eget viverra feugiat cras ut. Sit natoque montes tempus ligula eget vitae pede rhoncus maecenas consectetuer commodo condimentum aenean.</p>
+                                <h4>What are my payment options?</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                <blockquote>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                                    <p class="blockquote-footer">Someone famous in <cite title="Source Title">Dick Grayson</cite></p>
+                                </blockquote>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                            <div class="nav tag-cloud">
+                                <a href="#">Design</a>
+                                <a href="#">Development</a>
+                                <a href="#">Travel</a>
+                                <a href="#">Web Design</a>
+                                <a href="#">Marketing</a>
+                                <a href="#">Research</a>
+                                <a href="#">Managment</a>
+                            </div>
+                        </article>
+                        <div class="contact-form article-comment">
+                            <h4>Leave a Reply</h4>
+                            <form id="contact-form" method="POST">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input name="Name" id="name" placeholder="Name *" class="form-control" type="text">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input name="Email" id="email" placeholder="Email *" class="form-control" type="email">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea name="message" id="message" placeholder="Your message *" rows="4" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="send">
+                                            <button class="px-btn px-btn-white"><span>Submit</span> <i class="arrow"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- End Main -->
+    <!-- Footer Start -->
+    <footer class="footer white">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 m-10px-tb">
+                    <div class="nav justify-content-center justify-content-md-start">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fab fa-pinterest"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-6 m-10px-tb text-center text-md-right">
+                    <p>© 2020 copyright all right reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <a href="https://themeforest.net/item/nairo-bootstrap-4-personal-portfolio/27231134" class="fixd-btn" style="position: fixed; bottom: 20px; right: 20px; background: #bc0609; padding: 9px 34px; display: inline-block; color: #fff; font-weight: 600; border-radius: 60px;">
+        <span>Buy Now</span>
+    </a>
+
+    
+    <!-- jquery -->
+    <script src="static/js/jquery-3.2.1.min.js"></script>
+    <script src="static/js/jquery-migrate-3.0.0.min.js"></script>
+    <!-- end jquery -->
+    <!-- appear -->
+    <script src="static/plugin/appear/jquery.appear.js"></script>
+    <!-- end appear -->
+    <!--bootstrap-->
+    <script src="static/plugin/bootstrap/js/popper.min.js"></script>
+    <script src="static/plugin/bootstrap/js/bootstrap.js"></script>
+    <!--end bootstrap-->
+    <!-- End -->
+    <!-- custom js -->
+    <script src="static/js/custom.js"></script>
+    <!-- end -->
+</body>
+<!-- Body End -->
+
+</html>
